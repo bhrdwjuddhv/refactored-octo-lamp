288d0420b33d15b0aea4e4d4f086d105f969d11c
harishsehlangia <harish82817@gmail.com>
2026-01-19 12:02:12 +0530

    fix(env): fix configs of sever for Signup

--- frontend/brustOut/src/pages/signUP2.jsx
+++ frontend/brustOut/src/pages/signUP2.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
-const API = import.meta.env.VITE_SERVER_DOMAIN;
+const API = import.meta.env.VITE_API_URL;
 
 export default function SignUpPage() {
   const [formData, setFormData] = useState({
